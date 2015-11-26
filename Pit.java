5eeaf206fbdb6b58caf7e423840f2f442326b3b7
jerremyferrer <jerremyferrer@gmail.com>
2015-11-26 04:53:25 -0800

    commit testing

--- Pit.java
+++ Pit.java
@@ -13,20 +13,20 @@ public class Pit implements Icon {
 	private int height;
 	private int width;
 
-	@Override
+	@ Override
 	public void paintIcon(Component c, Graphics g, int x, int y) {
 		// TODO Complete third line
 		Graphics2D g2 = (Graphics2D) g;
 		g2.draw(new Ellipse2D.Double(x, y, width, height));
 		//Bead.draw........ 
 	}
-	@Override
+
 	public int getIconWidth() {
 		// TODO Auto-generated method stub
 		return width;
 	}
 
-	@Override
+	
 	public int getIconHeight() {
 		// TODO Auto-generated method stub
 		return height;
