cac3507558b1b314be7e1d79d30c51678630ea0f
Sydney Snyder <syd.sny@gmail.com>
2015-12-01 00:08:05 -0800

    Update BigPit.java

--- BigPit.java
+++ BigPit.java
@@ -2,10 +2,30 @@ package mancala;
 
 import java.awt.geom.Ellipse2D;
 
-public class BigPit extends Pit {
-	public BigPit() {
+public class BigPit extends Pit 
+{
+	/**
+	 * Initiates variables
+	 */
+	public BigPit() 
+	{
 		width = 70;
 		height = 130;
 		shape = new Ellipse2D.Float(0,0,width,height);
+		bead = new ArrayList<Beads>();
+	}
+	/**
+	 * Adds a new bead to the pit
+	 */
+	public void addBead()
+	{
+			bead.add(new bead);
+	}
+	/**
+	 * Removes last bead from pit
+	 */
+	public void removeBead()
+	{
+		bead.remove(bead.size());
 	}
 }
