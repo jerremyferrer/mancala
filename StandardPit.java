3a1159ee68d944c78f4f49f48ef37493e7a5c7a8
Sydney Snyder <syd.sny@gmail.com>
2015-12-02 22:19:30 -0800

    Update StandardPit.java

--- StandardPit.java
+++ StandardPit.java
@@ -1,30 +1,50 @@
-package mancala;
 
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
+import javax.swing.JPanel;
+/**
+ * Creates Pit ellipse
+ * @author Sydney Snyder, Jerremy Ferrer
+ *
+ */
 public class StandardPit extends Pit 
 {
-	public StandardPit() 
+	public StandardPit(Controller con) 
 	{
+		c = con;
 		width = 70;
 		height = 70;
+		x = 30;
+		y = 30;
 		shape = new Ellipse2D.Float(0, 0, width, height);
 		bead = new ArrayList<Beads>();
-		//if controller get bead amount picked = 4 then 
-		//do addBead() 4 times else 3 times
+	}
+	public void addInitialBeads()
+	{
+		int j = 0;
+		while(j < c.getBeadAmount())
+		{
+			this.addBead();
+			j++;
+		}
 	}
 	/**
-	 * Adds one new bead
+	 * Paints Pit with beads inside
 	 */
-	public void addBead()
+	public void paintIcon(Component c, Graphics g, int x, int y) 
 	{
-		bead.add(new beads);
-	}
-	/**
-	 * Removes last bead
-	 */
-	public void removeBead()
-	{
-		bead.remove(bead.size());
+		super.paintIcon(c, g, x, y);
+		for(int k = 0; k < bead.size(); k++)
+		{
+			bead.get(k).draw((Graphics2D) g);
+		}
 	}
 }
