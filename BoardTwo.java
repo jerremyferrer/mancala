79055a4c75b992114323619203694a40c19b6eb0
Sydney Snyder <syd.sny@gmail.com>
2015-12-02 20:54:02 -0800

    Update BoardTwo.java
    Sorry to destroy your simplicity lol I needed to add it so it worked with the same controller as start menu and what not. I'll make it simple again later, but just for the sake of you being able to see it run, I uploaded the long terrible version.

--- BoardTwo.java
+++ BoardTwo.java
@@ -1,24 +1,157 @@
-package mancala;
-
-import javax.swing.*;
-
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class BoardTwo extends BoardOne {
-	public BoardTwo() {
-		setColor();
-	}
-	public void setColor() {
-		displayPanel.setBackground(Color.GREEN);
-		pitB.setBackground(Color.GREEN);
-		pitA.setBackground(Color.GREEN);
-		Apits.setBackground(Color.GREEN);
-		Bpits.setBackground(Color.GREEN);
-	}
-	
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-	
 
+
+public class BoardTwo extends JFrame implements Board  {
+	private JPanel board;
+	private JPanel Apits;
+	private JPanel Bpits;
+	private JPanel pitA; //player A mancala store
+	private JPanel pitB; //player B mancala store
+	private JPanel displayPanel;
+	private JButton newGameBut;
+	private JLabel fillerSpace = new JLabel(" ");
+	private JLabel fillerSpace2 = new JLabel(" ");
+	private JLabel fillerSpace3 = new JLabel(" ");
+	private JLabel fillerSpace4 = new JLabel(" ");
+	private JLabel Alabel;
+	private JLabel Blabel;
+	
+	private Controller c;
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 300;
+	/**
+	 * Initiates BoardTwo with a controller
+	 * @param con - the controller for the board
+	 */
+	public BoardTwo(Controller con) 
+	{
+		c = con;
+		this.setTitle("Mancala");
+		board = new JPanel(new BorderLayout());
+		board.setBackground(Color.GREEN);
+		initializeBoard();
+	}
+	/**
+	 * Draws the Board
+	 */
+	public void initializeBoard() 
+	{
+		initializeMainPitB();
+		initializeMainPitA();
+		initializeAPits();
+		initializeBPits();
+		initializeCenterBoard();
+		this.setColor();
+		this.setResizable(false);
+		this.getContentPane().add(board);
+		this.setLocation(600,200);
+		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);	
+	}
+	/**
+	 * Creates and draws pits for Player A
+	 */
+	public void initializeAPits() 
+	{
+		Apits = new JPanel(new GridLayout());
+		Apits.add(fillerSpace3);
+		for(int i = 0; i < 6; i++) 
+		{
+			StandardPit stPit = new StandardPit(c);
+			c.addPitsA(stPit);
+			stPit.addInitialBeads();
+			Apits.add(new JLabel(stPit));
+		}
+		Apits.add(fillerSpace4);
+		board.add(Apits, BorderLayout.SOUTH);
+	}
+	/**
+	 * Creates and draws pits for Player B
+	 */
+	public void initializeBPits() 
+	{
+		Bpits = new JPanel(new GridLayout());
+		Bpits.add(fillerSpace);
+		for(int i = 0; i < 6; i++) 
+		{
+			StandardPit stPit = new StandardPit(c);
+			c.addPitsB(stPit);
+			stPit.addInitialBeads();
+			Bpits.add(new JLabel(stPit));
+		}
+		Bpits.add(fillerSpace2);
+		board.add(Bpits, BorderLayout.NORTH);
+	}
+	/**
+	 * Creates and draws the main pit for Player A
+	 */
+	public void initializeMainPitA() 
+	{
+		pitA = new JPanel();
+		Alabel = new JLabel("A");
+		BigPit bPit = new BigPit();
+		pitA.add(Alabel);
+		pitA.add(new JLabel(bPit));
+		board.add(pitA, BorderLayout.EAST);
+	}
+	/**
+	 * Creates and draws the main pit for Player B
+	 */
+	public void initializeMainPitB() 
+	{
+		pitB = new JPanel();
+		Blabel = new JLabel("B");
+		BigPit bPit = new BigPit();
+		pitB.add(new JLabel(bPit));
+		pitB.add(Blabel);
+		board.add(pitB, BorderLayout.WEST);
+	}
+	/**
+	 * Creates and Initializes the Center of the Board
+	 */
+	public void initializeCenterBoard() 
+	{
+		displayPanel = new JPanel();
+		newGameBut = new JButton("New Game");
+		newGameBut.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				StartMenu s = new StartMenu();
+				closeGameFrame();
+			}
+		});
+		displayPanel.add(newGameBut);
+		board.add(displayPanel, BorderLayout.CENTER);
+	}
+	/**
+	 * Closes the Mancalla frame
+	 */
+	public void closeGameFrame() 
+	{
+		this.dispose();
+	}
+	/**
+	 * Sets the color of the board
+	 */
+	@Override
+	public void setColor() 
+	{
+		displayPanel.setBackground(Color.CYAN);
+		pitB.setBackground(Color.CYAN);
+		pitA.setBackground(Color.CYAN);
+		Apits.setBackground(Color.CYAN);
+		Bpits.setBackground(Color.CYAN);
+	}
 }
