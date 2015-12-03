1eac22709323f4ce5ef8a2233b58b79368fdc6ee
Sydney Snyder <syd.sny@gmail.com>
2015-12-02 22:18:40 -0800

    Update Pit.java

--- Pit.java
+++ Pit.java
@@ -1,23 +1,30 @@
-package mancala;
 
 import java.awt.Color;
 import java.awt.Component;
-
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 import javax.swing.Icon;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-public abstract class Pit implements Icon, ChangeListener {
+/**
+ * Creates abstract class Pit
+ * @author Sydney Snyder, Jerremy Ferrer, & Royce Florence Rocco
+ *
+ */
+public abstract class Pit implements Icon, ChangeListener 
+{
 	protected Ellipse2D shape;
+	protected final int BEADSIZE = 10;
 	protected int x;
 	protected int y;
 	protected int width;
 	protected int height;
+	protected Controller c;
 	protected ArrayList<Beads> bead;
 	
 	//no constructor b/c abstract class
@@ -30,7 +37,24 @@ public abstract class Pit implements Icon, ChangeListener {
 		g2.setColor(Color.BLACK);
 		g2.draw(new Ellipse2D.Float(x, y, width, height));
 	}
-
+	public void addBead()
+	{
+		bead.add(new Beads(x, y));
+		if(x < width)
+			x += BEADSIZE+2;
+		else
+		{
+			y += BEADSIZE+2;
+			x = 0;
+		}
+	}
+	/**
+	 * Removes last bead
+	 */
+	public void removeBead()
+	{
+		bead.remove(bead.size());
+	}
 	public int getIconWidth() {
 		// TODO Auto-generated method stub
 		return width;
@@ -43,8 +67,16 @@ public abstract class Pit implements Icon, ChangeListener {
 	}
 
 	@Override
-	public void stateChanged(ChangeEvent e) {
-		// TODO Auto-generated method stub
+	public void stateChanged(ChangeEvent e) 
+	{
 		
+	}
+	public boolean contains(Point2D p)
+	{
+		return shape.contains(p);
+	}
+	public int getBeadSize()
+	{
+		return bead.size();
 	}
 }
