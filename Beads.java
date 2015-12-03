7c9fc5eed9111596f980ae085cd40b1ffd0df71d
Sydney Snyder <syd.sny@gmail.com>
2015-12-02 22:13:13 -0800

    Update Beads.java

--- Beads.java
+++ Beads.java
@@ -1,31 +1,23 @@
-package mancala;
-
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 
-public class Beads 
+import javax.swing.JComponent;
+
+public class Beads extends JComponent
 {
-	private final int width = 5;
-	private final int height = 5;
-	private int i = 0;
-	private int x = 5;
-	private int y = 5;
+	private final int width = 10;
+	private final int height = 10;
+	private int x;
+	private int y;
+	private Ellipse2D bead;
 	public Beads(int x1, int y1)
 	{
 		x = x1;
 		y = y1;
+		bead = new Ellipse2D.Float(x,y,width,height);
 	}
 	public void draw(Graphics2D g2)
 	{
-		Ellipse2D bead = new Ellipse(x, y, width, height);
-		//This next portion will probably have to be done in the pit
-		//portion of our code or wherever a pit is created. 
-		//Beads should ask to input x1 and y1 so it can vary from
-		//pit to pit.
-		i+=1;
-		if(i%2 != 0)
-			x += width;
-		else
-			y += height;
-		g2.draw(Bead);
+		g2.draw(bead);
 	}
 }
