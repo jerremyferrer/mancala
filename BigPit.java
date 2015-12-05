f4994f00b018e38105fbb6bfa4cc4c7a89cfc4e3
Sydney Snyder <syd.sny@gmail.com>
2015-12-04 22:57:12 -0800

    Update BigPit.java

--- BigPit.java
+++ BigPit.java
@@ -2,6 +2,7 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class BigPit extends Pit 
@@ -13,18 +14,11 @@ public class BigPit extends Pit
 	{
 		width = 70;
 		height = 130;
+		x1 = 20;
+		y1 = 10;
+		x = 20;
+		y = 10;
 		shape = new Ellipse2D.Float(0,0,width,height);
 		bead = new ArrayList<Beads>();
-	}
-	/**
-	 * Adds beads into paint
-	 */
-	public void paintIcon(Component c, Graphics g, int x, int y) 
-	{
-		super.paintIcon(c, g, x, y);
-		for(int k = 0; k < bead.size(); k++)
-		{
-			bead.get(k).draw((Graphics2D) g);
-		}
 	}
 }
