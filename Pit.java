841e4fa54aab1d3ba95807bf06d24b39ca391a62
Sydney Snyder <syd.sny@gmail.com>
2015-12-04 23:02:37 -0800

    Update Pit.java

--- Pit.java
+++ Pit.java
@@ -19,11 +19,10 @@ import javax.swing.event.ChangeListener;
 public abstract class Pit implements Icon, ChangeListener 
 {
 	protected Ellipse2D shape;
+	protected Player p;
 	protected final int BEADSIZE = 10;
-	protected int x;
-	protected int y;
-	protected int width;
-	protected int height;
+	protected boolean ifXagain = false;
+	protected int x, y, x1, y1, width, height;
 	protected Controller c;
 	protected ArrayList<Beads> bead;
 	
@@ -36,41 +35,92 @@ public abstract class Pit implements Icon, ChangeListener
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.BLACK);
 		g2.draw(new Ellipse2D.Float(x, y, width, height));
+		for(int k = 0; k < bead.size(); k++)
+		{
+			bead.get(k).draw(g2);
+		}
 	}
+	/**
+	 * Adds bead and updates x
+	 * (x is updated so that beads 
+	 * are put in the proper place inside
+	 * the pit)
+	 */
 	public void addBead()
 	{
-		bead.add(new Beads(x, y));
-		if(x < width)
+		if(ifXagain == true)
+		{
+			x = x1;
+			y = y1;
+			bead.add(new Beads(x, y));
+			ifXagain = false;
+		}
+		else if(x < width)
+		{
+			bead.add(new Beads(x, y));
 			x += BEADSIZE+2;
+		}
 		else
 		{
+			bead.add(new Beads(x, y));
 			y += BEADSIZE+2;
-			x = 0;
+			x = x1;//how far in it needs to be from edge to be visible
 		}
 	}
 	/**
 	 * Removes last bead
+	 * (x is updated backwards of add method 
+	 * so beads are put in proper place)
 	 */
 	public void removeBead()
 	{
-		bead.remove(bead.size());
+		if(bead.size() == 1)
+		{
+			ifXagain = true;
+			bead.remove(bead.size()-1);
+		}
+		else if(bead.size() == 0)
+		{
+			x = x1;
+			y = y1;
+		}
+		else
+		{
+			bead.remove(bead.size() - 1);
+			if(x > x1)
+				x -= BEADSIZE + 2;
+			else if(x != x1)
+				y -= BEADSIZE + 2;
+		}
 	}
 	public int getIconWidth() {
 		// TODO Auto-generated method stub
 		return width;
 	}
 
-	
+	public Player getPlayer()
+	{
+		return p;
+	}
 	public int getIconHeight() {
 		// TODO Auto-generated method stub
 		return height;
 	}
-
+	public Ellipse2D getShape()
+	{
+		return shape;
+	}
 	@Override
 	public void stateChanged(ChangeEvent e) 
 	{
-		
+	//probably didn't need this	
 	}
+	/**
+	 * Checks to see if the ellipse contains the 
+	 * clicked point)
+	 * @param p - point
+	 * @return true or false
+	 */
 	public boolean contains(Point2D p)
 	{
 		return shape.contains(p);
@@ -78,5 +128,9 @@ public abstract class Pit implements Icon, ChangeListener
 	public int getBeadSize()
 	{
 		return bead.size();
+	}
+	public String toString()
+	{
+		return "Pit[bead size = " + bead.size() + ", Location =" + x +", " + y + "]";
 	}
 }
