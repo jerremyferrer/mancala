c24389d6abad7e583dd97d15b8387b5aa91b8a57
Sydney Snyder <syd.sny@gmail.com>
2015-12-05 16:05:00 -0800

    Update Beads.java

--- Beads.java
+++ Beads.java
@@ -3,7 +3,11 @@ import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
 import javax.swing.JComponent;
-
+/**
+ * Establishes beads for Mancala game
+ * @author Sydney Snyder, Jerremy Ferrer, & Royce Florence Rocco
+ *
+ */
 public class Beads extends JComponent
 {
 	private final int width = 10;
@@ -21,5 +25,13 @@ public class Beads extends JComponent
 	{
 		g2.fill(bead);
 		g2.draw(bead);
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
 	}
 }
